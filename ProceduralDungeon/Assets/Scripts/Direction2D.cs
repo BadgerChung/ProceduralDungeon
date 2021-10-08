5d8bf21320395db8a09bd00f911070c67066fc1b
BadgerChung <jirka.dal@gmail.com>
2021-10-08 21:35:04 +0200

    Přidání Random Walk algoritmu

---
+++ ProceduralDungeon/Assets/Scripts/Direction2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class Direction2D
+{
+    public static List<Vector2Int> directionsList = new List<Vector2Int>
+    {
+        new Vector2Int(0, 1), // nahoru
+        new Vector2Int(0, -1), // dolů
+        new Vector2Int(1, 0), // doprava
+        new Vector2Int(-1, 0) // doleva
+    };
+
+    public static Vector2Int GetRandomDirection()
+    {
+        return directionsList[Random.Range(0, directionsList.Count)];
+    }
+}
---
+++ ProceduralDungeon/Assets/Scripts/ProceduralGenerationAlgorithms.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProceduralGenerationAlgorithms
+{
+    public static HashSet<Vector2Int> RandomWalk(Vector2Int startPosition, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(startPosition);
+
+        Vector2Int previousPosition = startPosition;
+
+        for (int i = 0; i < walkLength; i++)
+        {
+            Vector2Int newPosition = previousPosition + Direction2D.GetRandomDirection();
+            path.Add(newPosition);
+            previousPosition = newPosition;
+        }
+
+        return path;
+    }
+}
